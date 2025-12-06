26049093b2e31d23e96e88ce1d176a67fee43309
Nico Vromans <VromansN+Github@pm.me>
2025-12-06 14:36:08 +0100

    Added year 2025 - day 6 - part 1 (not solved)
    TODO: solve 2025-06.1

--- year2025/src/lib.rs
+++ year2025/src/lib.rs
@@ -5,6 +5,7 @@ pub mod day02;
 pub mod day03;
 pub mod day04;
 pub mod day05;
+pub mod day06;
 
 pub fn get_solver(day: u8) -> Option<Box<dyn Solver>> {
     match day {
@@ -13,6 +14,7 @@ pub fn get_solver(day: u8) -> Option<Box<dyn Solver>> {
         3 => Some(Box::new(day03::Day03)),
         4 => Some(Box::new(day04::Day04)),
         5 => Some(Box::new(day05::Day05)),
+        6 => Some(Box::new(day06::Day06)),
         _ => None,
     }
 }
